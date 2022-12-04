3021d313ae2b1f58e548a33cc13fbb40102533db
yanastasevich <ioannstasevich1@gmail.com>
2022-12-04 19:39:21 +0100

    final version

--- src/main/java/CRT.java
+++ src/main/java/CRT.java
@@ -1,30 +1,51 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 class CRT {
 
     private BigInteger p;
     private BigInteger q;
-    private BigInteger z;
 
-   public CRT(BigInteger p, BigInteger q, BigInteger z){
-       this.p = p;
-       this.q = q;
-       this.z = z;
-   }
+    public CRT(BigInteger p, BigInteger q) {
+        this.p = p;
+        this.q = q;
+    }
 
-   BigInteger pow(BigInteger basis, BigInteger exponent){
-       BigInteger a = basis.pow(exponent.mod(p.subtract(BigInteger.ONE)).intValue()).mod(p);
-       BigInteger b = basis.pow(exponent.mod(q.subtract(BigInteger.ONE)).intValue()).mod(q);
+    BigInteger pow(BigInteger basis, BigInteger exponent) {
+        BigInteger z = q.modInverse(p);
+        BigInteger a = basis.modPow(exponent, p);
+        BigInteger b = basis.modPow(exponent, q);
 
-       BigInteger v = a.subtract(b).multiply(z).mod(p);
-       return q.multiply(v).add(b);
-   }
+        BigInteger v = a.subtract(b).multiply(z).mod(p);
+        return q.multiply(v).add(b);
+    }
 
+    public int[] generateRandomParams(BigInteger n) {
+        Random random = new Random();
+        int x = random.ints(1, 2, n.subtract(BigInteger.ONE).intValue()).findFirst().getAsInt();
+        int y = random.ints(1, 2, n.subtract(BigInteger.ONE).intValue()).findFirst().getAsInt();
+        return new int[]{x, y};
+    }
 
-    public static void main(String args[])
-    {
-        CRT crt = new CRT(BigInteger.valueOf(101), BigInteger.valueOf(103), BigInteger.valueOf(51));
-        BigInteger potenzResult = crt.pow(BigInteger.valueOf(500), BigInteger.valueOf(3000));
-        System.out.println("potenz is equal to: " + potenzResult);
+    public static void main(String args[]) {
+        CRT crt = new CRT(BigInteger.valueOf(101), BigInteger.valueOf(103));
+
+        int[] arrayXy = crt.generateRandomParams(crt.p.multiply(crt.q));
+        BigInteger x = BigInteger.valueOf(arrayXy[0]);
+        BigInteger y = BigInteger.valueOf(arrayXy[1]);
+        System.out.println("random x: " + x);
+        System.out.println("random y: " + y);
+
+        // pow method
+        long startTime1 = System.nanoTime();
+        BigInteger powResult = crt.pow(x, y);
+        long finishTime1 = System.nanoTime();
+        System.out.println("Pow method with the result = " + powResult + " took: " + (finishTime1 - startTime1) + " nanoseconds");
+
+        // modPow method
+        long startTime2 = System.nanoTime();
+        BigInteger modPowResult = x.modPow(y, crt.p.multiply(crt.q));
+        long finishTime2 = System.nanoTime();
+        System.out.println("ModPow method with the result = " + modPowResult + " took: " + (finishTime2 - startTime2) + " nanoseconds");
     }
 }
--- src/test/java/ChineseRemainderTheoremTest.java
+++ src/test/java/ChineseRemainderTheoremTest.java
@@ -8,15 +8,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ChineseRemainderTheoremTest {
 
     @ParameterizedTest
-    @CsvSource({"101, 103, 51, 500, 3000"})
-    public void testPowMethod(int p, int q, int z, int x, int y) {
+    @CsvSource({"101, 103, 500, 3000"})
+    public void testPowMethod(int p, int q, int x, int y) {
         BigInteger bigIntP = BigInteger.valueOf(p);
         BigInteger bigIntQ = BigInteger.valueOf(q);
-        BigInteger bigIntZ = BigInteger.valueOf(z);
         BigInteger bigIntX = BigInteger.valueOf(x);
         BigInteger bigIntY = BigInteger.valueOf(y);
 
-        CRT crt = new CRT(bigIntP, bigIntQ, bigIntZ);
+        CRT crt = new CRT(bigIntP, bigIntQ);
 
         BigInteger actualResult = crt.pow(bigIntX, bigIntY);
         BigInteger expectedResult = BigInteger.valueOf(4546);
@@ -24,6 +23,24 @@ public class ChineseRemainderTheoremTest {
         assertEquals(actualResult, expectedResult,
                 "Actual result of the pow method: " + actualResult + " should be the same as the expected result: "
                         + expectedResult);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"101, 103, 500, 3000"})
+    public void testPowMethodWithModPow(int p, int q, int x, int y) {
+        BigInteger bigIntP = BigInteger.valueOf(p);
+        BigInteger bigIntQ = BigInteger.valueOf(q);
+        BigInteger bigIntX = BigInteger.valueOf(x);
+        BigInteger bigIntY = BigInteger.valueOf(y);
+
+        CRT crt = new CRT(bigIntP, bigIntQ);
+
+        BigInteger exponentWithPow = crt.pow(bigIntX, bigIntY);
+        BigInteger exponentWithModPow = bigIntX.modPow(bigIntY, bigIntP.multiply(bigIntQ));
+
+        assertEquals(exponentWithPow, exponentWithModPow,
+                "The result of the pow method: " + exponentWithPow + " should be the same as the result of modPow method: "
+                        + exponentWithModPow);
     }
 
 }
